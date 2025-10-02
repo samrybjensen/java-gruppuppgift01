8bb772a5b3052b378e838300db42e1b0c17866c7
Samuel Ryberg <samuel.ryberg@gmail.com>
2025-10-02 14:50:40 +0200

    fixed compile error from missing list import

--- src/test/java/dev/jensendev25/groupassignment/LibraryTest.java
+++ src/test/java/dev/jensendev25/groupassignment/LibraryTest.java
@@ -3,6 +3,8 @@ package dev.jensendev25.groupassignment;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import dev.jensendev25.groupassignment.junit.*;
 
